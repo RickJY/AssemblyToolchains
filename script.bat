cbad5b2baa9e50fb8d679e3d1af05da6b337469b
RickJY <98106012+RickJY@users.noreply.github.com>
2022-10-30 23:56:08 +0800

    script on windows
    This script gets tools on windows and uses the CMD to finish some steps, but I don't how to run the .sh file on windows. So it just installs the files on windows.

---
+++ script.bat
@@ -0,0 +1,12 @@
+@echo hello world!
+@echo This program will clone the file on current path
+@pause
+@git clone https://github.com/LubosKuzma/AssemblyToolchains.git
+@cd AssemblyToolchains
+@cd scripts
+@echo %cd%
+@pause
+@dir
+@pause
+@echo You have installed the tools
+@pause
