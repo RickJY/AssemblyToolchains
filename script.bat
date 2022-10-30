0d4882baad2243545c8d50b42b79fda63effdc63
RickJY <98106012+RickJY@users.noreply.github.com>
2022-10-31 04:48:22 +0800

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,7 +1,12 @@
 @echo hello world!
 @echo This program will clone the file on current path
 @pause
+@d:
+@dir
+@pause
 @git clone https://github.com/LubosKuzma/AssemblyToolchains.git
+@setx "path" "D:D:\AssemblyToolchains\scripts;%path%"
+@pause
 @cd AssemblyToolchains
 @cd scripts
 @echo %cd%
