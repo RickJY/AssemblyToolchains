c932b11110817a511197673d7716b1fbbef6612c
RickJY <98106012+RickJY@users.noreply.github.com>
2022-11-04 04:30:46 +0800

    Update script.bat
    The script will ask the user where they want to install them. After that, it will be not to use any command that the windows does not come with. In the end, the script will set the system environment variable.

--- script.bat
+++ script.bat
@@ -1,21 +1,21 @@
-@echo hello, welcome to use the script to install the AssemblyToolchains 
-@echo This program will clone the file on Temp files (you can use the %%temp%% in address bar to find it)
-@pause
-@c:
-@cd %temp%
-@echo %cd%
-@pause
-@echo we will clone from github and add them in temp files
-@pause
-@git clone https://github.com/LubosKuzma/AssemblyToolchains.git
-@setx "path" "%temp%\AssemblyToolchains\scritps\arm_toolchain.sh;%path%"
-@setx "path" "%temp%\AssemblyToolchains\scritps\x86_toolchain.sh;%path%"
-@pause
-@cd AssemblyToolchains
-@cd scripts
-@echo %cd%
-@pause
-@dir
-@pause
-@echo You have installed the tools
-@pause
+@echo off
+echo hello world!
+echo This program will download the install files on github
+
+:st
+set input=
+set /p input=please enter path which you want to save:
+echo you enter: %input%
+pause
+if exist %input% ( echo we will do next step
+) else ( echo the path is not exist, please enter a right path 
+goto :st)
+
+cd %input%
+pause
+certutil -urlcache -split -f https://raw.githubusercontent.com/LubosKuzma/AssemblyToolchains/main/scripts/arm_toolchain.sh %input%\arm_toolchain.sh
+certutil -urlcache -split -f https://raw.githubusercontent.com/LubosKuzma/AssemblyToolchains/main/scripts/x86_toolchain.sh %input%\x86_toolchain.sh
+setx "Path" "%input%\arm_toolchain.sh%;%path%" /m
+setx "Path" "%input%\x86_toolchain.sh%;%path%" /m
+echo you have got the Toolchains
+pause
